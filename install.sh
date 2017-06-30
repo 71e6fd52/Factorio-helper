979f591d11926af3caa3d3ad514b92274b0b3718
745275633 <DAStudioDirector@gmail.com>
2017-06-30 10:01:05 +0800

    complate

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ bin=$share/bin
 xmake install --installdir=$share --root
 install -d $output/bin
 install -d $bin
+install -d $output/share/zsh/site-functions
 install -m755 src/factorio-helper $output/bin
 install -m755 src/add $bin
 install -m755 src/change $bin
@@ -17,3 +18,4 @@ install -m755 src/list $bin
 install -m755 src/pull $bin
 install -m755 src/push $bin
 install -m755 src/update $bin
+install -m755 _factorio-helper $output/share/zsh/site-functions
