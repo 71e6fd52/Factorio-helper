2900ecd3cbfabeca8b323139fa28a0aa2a477be9
745275633 <DAStudioDirector@gmail.com>
2017-07-01 14:55:32 +0800

    optimization

--- src/search.cpp
+++ src/search.cpp
@@ -3,13 +3,17 @@
 int main()
 {
 	using namespace std;
-	cout << "Mod name: ";
 	string name;
-	cin >> name;
 	factorio::mod::info mod;
-	mod.search(name);
-	cout << "Name: " << mod.name() << endl
-		<< "Title: " << mod.title() << endl
-		<< "Licesne: " << mod.license_name() << endl
-		<< mod.summary() << endl;
+	while(true)
+	{
+		cerr << "Mod name: ";
+		if(!getline(cin, name))
+			break;
+		mod.search(name);
+		cout << "    Name: " << mod.name() << endl
+			<< "    Title: " << mod.title() << endl
+			<< "    Licesne: " << mod.license_name() << endl
+			<< "    " << mod.summary() << endl;
+	}
 }
