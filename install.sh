024295ff76b59227654efb8ce6e02d98b373a3c9
745275633 <DAStudioDirector@gmail.com>
2017-08-20 17:51:04 +0800

    delete

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ install -d $output/share/zsh/site-functions
 install -m755 src/factorio-helper $output/bin
 install -m755 src/add $bin
 install -m755 src/change $bin
+install -m755 src/delete $bin
 install -m755 src/download $bin
 install -m755 src/init $bin
 install -m755 src/list $bin
